c58fd1be55c2d7a8c83abb79c802c0a6cd9bb750
cfly <cfly.hi@gmail.com>
2015-02-22 00:46:54 +0800

    Initial Commit

---
+++ podsampleTests/podsampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  podsampleTests.swift
+//  podsampleTests
+//
+//  Created by cfly on 15/2/22.
+//  Copyright (c) 2015年 cfly. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class podsampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
