7f821e4741cf1a8a51d937debe944b5aa4fe4c0b
cfly <caofei.org@gmail.com>
2015-02-27 11:18:16 +0800

    add cocoa pods support

---
+++ podsample/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  podsample
+//
+//  Created by cfly on 15/2/26.
+//  Copyright (c) 2015年 cfly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ podsample/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  podsample
+//
+//  Created by cfly on 15/2/26.
+//  Copyright (c) 2015年 cfly. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
